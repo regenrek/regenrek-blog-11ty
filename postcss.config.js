b7c9af21c6fac50f12105aaaf9f6fbe0338a1775
Kevin Regenrek <hello@regenrek.at>
2020-04-20 16:32:52 +0200

    cleanup add purgecss after build

--- postcss.config.js
+++ postcss.config.js
@@ -9,13 +9,6 @@ const plugins = [
   }))
 ];
 
-// plugins.push(
-//   require("postcss-preset-env")({
-//     browsers: 'last 2 versions',
-//     stage: '0' // importat for @custom-media (default === 2)
-//   })
-// )
-
 if (process.env.NODE_ENV === "production") {
   // plugins.push(
   //   require("@fullhuman/postcss-purgecss")({
---
+++ purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./dist/**/*.html"],
+    css: ["./dist/assets/*.css"],
+    extractors: [
+      {
+        extractor: content => {
+          return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+        },
+        extensions: ["html"]
+      }
+    ]
+  };
