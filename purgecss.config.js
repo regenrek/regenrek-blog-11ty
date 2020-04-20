cbf4946ff889ff91fac91908d64c80914feac48b
Kevin Regenrek <hello@regenrek.at>
2020-04-20 17:32:27 +0200

    fix regex

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     content: ["./dist/**/*.html"],
     css: ["./dist/assets/*.css"],
-    whitelistPatterns: ['/cc-/', 'cookieconsent'],
-    whitelistPatternsChildren: ['/cc-/', 'cookieconsent'],
+    whitelistPatterns: [/^cc-/, /cookieconsent/],
+    whitelistPatternsChildren: [/^cc-/, /cookieconsent/],
     extractors: [
       {
         extractor: content => {
